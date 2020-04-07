0ce18a5f62efce58313ca953c03362bf5857d21a
Vlatko Ploskoski <wiseraven23@yahoo.com>
2020-04-07 13:10:34 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow (aura)# Write your #display_rainbow method here
-puts "R:#{aura(0)}", "O:{aura (1)}", "Y:{aura (2)}", "G:{aura (3)}," "B:{aura (4)}," "I:{aura (5)}," "V:{aura (6)}" 
+puts "R:#{aura(0)}", "O:{aura (1)}", "Y:{aura (2)}", "G:{aura (3)}," "B:{aura (4)}," "I:{aura (5)}," "V:{aura (6)}"
 end 
